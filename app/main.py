3484bd7de95376b28298728772714957709bffea
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-06-03 16:35:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,11 +5,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command=="exit":
+        if command == "exit":
             break
         if command.startswith("echo "):
             print(command[5:])
         else:
             print(f"{command}: command not found")
+
+
 if __name__ == "__main__":
     main()
