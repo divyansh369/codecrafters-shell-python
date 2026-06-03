08b3b7a79d1fbe85e5e8ff67594c0021c64a4889
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-06-03 16:32:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ def main():
         command = input()
         if command=="exit":
             break
-        print(f"{command}: command not found")
-
+        if command.startswith("echo "):
+            print(command[5:])
+        else:
+            print(f"{command}: command not found")
 if __name__ == "__main__":
     main()
