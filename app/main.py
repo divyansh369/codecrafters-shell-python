895e9f1fdfeddbf7773992af6a54b7bd042e0f77
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-06-05 01:51:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,25 +1,40 @@
-import cmd
+import shlex
 import sys
 import shutil
 
 def main():
+    BUILTIN = ["echo", "type", "exit"]
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
+        sys.stdout.flush()
+
+        try:
+            user_input = input()
+        except EOFError or KeyboardInterrupt:
+            print()
             break
-        if command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            cmd = command[5:]
-            if cmd in ["echo", "type","exit"]:
-                print(f"{command[5:]} is a shell builtin")
-            elif shutil.which(cmd) is not None:
-                print(f"{command[5:]} is {shutil.which(cmd)}")
-            else:
-                print(f"{command[5:]}: not found")
+        
+        if not user_input:
+            continue
+        
+        args = shlex.split(user_input)
+        cmd = args[0]
+
+        if cmd == "exit":
+            break
+    
+        if cmd == "echo":
+            print(" ".join(args[1:]))
+        
+        elif cmd == "type":
+            if args[1] in BUILTIN:
+                print(f"{args[1]} is a shell builtin")
+            elif shutil.which(args[1]) is not None:
+                print(f"{args[1]} is {shutil.which(args[1])}")
+            else: 
+                print(f"{args[1]}: not found")
         else:
-            print(f"{command}: command not found")
+            print(f"{cmd}: command not found")
 
 
 if __name__ == "__main__":
